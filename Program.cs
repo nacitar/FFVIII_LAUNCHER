12f48bbbad9ab52dcd68f6861b81d3a48ff2688e
Jacob McIntosh <nacitar@ubercpp.com>
2022-11-28 22:32:49 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Diagnostics;
+
+string fullBinaryPath = Path.Combine(
+    Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location) ?? Directory.GetCurrentDirectory(),
+    "FFVIII.exe"
+);
+using (Process myProcess = new Process())
+{
+    myProcess.StartInfo = new() {
+        UseShellExecute = false,
+        FileName = fullBinaryPath,
+        CreateNoWindow = true
+    };
+    Console.WriteLine($"Starting {Path.GetFileName(fullBinaryPath)}: {fullBinaryPath}");
+    myProcess.Start();
+}
